96615341e05729028b83af2583eea905fddf1cdf
Nllii <ch1ris23@ymail.com>
2022-05-23 17:05:23 -0500

    Update

--- _scripts/upload_caprover.sh
+++ _scripts/upload_caprover.sh
@@ -1,7 +1,7 @@
 RED='\033[0;31m'
 White="\[\033[0;37m\]"
 END='\033[0m' 
-IYellow="[\033[0;93m -]"     
+IYellow="\033[0;93m[x]"     
 # echo -e "${RED}Uploading to caprover... \nI have no control after this point...${END}"
 account_=$(<credentials.txt)
 _name=$(echo $account_ | cut -d " " -f2)
@@ -11,8 +11,8 @@ cluster_password=${_password#*=}
 _domain=$(echo $account_ | cut -d " " -f3)
 cluster_domain=${_domain#*=}
 
-echo  "${IYellow}Cluster name: $cluster_name${END}"
-echo  "${IYellow}Cluster password: $cluster_password${END}"
-echo  "${IYellow}Cluster domain: $cluster_domain${END}"
+echo -e "${IYellow}Cluster name: $cluster_name${END}"
+echo -e "${IYellow}Cluster password: $cluster_password${END}"
+echo  -e "${IYellow}Cluster domain: $cluster_domain${END}"
 
 caprover deploy -h $cluster_domain -p $cluster_password -b main -a $cluster_name
