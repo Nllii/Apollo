ca05da048be10717bb0c2202c17cfee326c43622
Nllii <ch1ris23@ymail.com>
2022-05-23 01:25:56 -0500

    Update

--- developer.py
+++ developer.py
@@ -1,8 +1,14 @@
 from pick import pick
 import os
 import subprocess
+import sys, select
+
 shell_scripts_dir = os.path.dirname(os.path.abspath(__file__))
 shell_scripts= os.path.join(shell_scripts_dir, '_scripts')
+
+
+
+
 
 def repo_task():
     title = 'REPO TASK: '
@@ -14,7 +20,8 @@ def repo_task():
 
 
 def _admin_task():
-    title = 'DEVELOPMENT TASK: '
+    title = 'AUTO DEPLOYMENT AFTER 3 SECONDS TO GITHUB ...\nDEVELOPMENT TASK: '
+
     options = ['git init','commit']
     option, index = pick(options, title, indicator='=>', default_index=0)
     return option, index
@@ -22,13 +29,26 @@ def _admin_task():
 
 
 
+# if (i):
+#   print ("You said", sys.stdin.readline().strip())
+# else:
+#   print ("You said nothing!")
 
 
+
+
+
+x_time, o, e = select.select( [sys.stdin], [], [],  3)
+subprocess.call(['bash', shell_scripts + '/commit_project.sh'])
 option, index = _admin_task()
-if index == 0:
-    repo_option, repo_index = repo_task()
-    if repo_index == 0:
-        subprocess.call(['bash', shell_scripts + '/scan_project.sh'])
+# if (x_time):
+# else:
+#     print("You")
+
+# if index == 0:
+#     repo_option, repo_index = repo_task()
+#     if repo_index == 0:
+#         subprocess.call(['bash', shell_scripts + '/scan_project.sh'])
     
-elif index == 1:
-    subprocess.call(['bash', shell_scripts + '/commit_project.sh'])
+# elif index == 1:
+#     subprocess.call(['bash', shell_scripts + '/commit_project.sh'])
