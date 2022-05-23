be338aa6c7553518209a987b1e92a53bba55db68
Nllii <ch1ris23@ymail.com>
2022-05-23 03:05:37 -0500

    Update

--- _scripts/upload_caprover.sh
+++ _scripts/upload_caprover.sh
@@ -1,3 +1,6 @@
 RED='\033[0;31m'
 END='\033[0m' # No Color
-echo -e "${RED}Uploading caprover.zip to S3...${END}"
+echo -e "${RED}Uploading to caprover... \nI have no control after this point...${END}"
+cluster_name=''
+caprover_password=''
+caprover deploy -h http://captain.me.cloudships.me -p $caprover_password -b main -a $cluster_name
--- developer.py
+++ developer.py
@@ -4,6 +4,14 @@ import subprocess
 shell_scripts_dir = os.path.dirname(os.path.abspath(__file__))
 shell_scripts= os.path.join(shell_scripts_dir, '_scripts')
 
+def building():
+    title = 'BUILDING TASK: '
+    options = ['upload to caprover']
+    option, index = pick(options, title, indicator='=>', default_index=0)
+    return option, index
+
+
+
 def repo_task():
     title = 'REPO TASK: '
     options = ['create a new repo and push', 'caprover']
@@ -34,19 +42,21 @@ if index == 0:
         subprocess.call(['bash', shell_scripts + '/start_django.sh'])
     else:
         pass
-    if repo_index == 1:
+
+    if repo_index == 3:
         subprocess.call(['bash', shell_scripts + '/upload_caprover.sh'])
     else:
         pass
-
-
-
-
 elif index == 1:
     subprocess.call(['bash', shell_scripts + '/commit_project.sh'])
 
 
-# elif index == 3:
-#     subprocess.call(['bash', shell_scripts + '/upload_caprover.sh'])
+elif index == 3:
+    option, index = building()
+    if index == 0:
+        subprocess.call(['bash', shell_scripts + '/upload_caprover.sh'])
 
     
+
+
+
