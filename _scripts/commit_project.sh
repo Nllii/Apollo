f20c73bc9536faea59d34703a0e85d2965cb5739
Nii Golightly <nllii@pm.me>
2022-05-26 19:26:52 +0000

    update

--- _scripts/commit_project.sh
+++ _scripts/commit_project.sh
@@ -4,10 +4,12 @@ END='\033[0m'
 IYellow="\033[0;93m[x]" 
 
 function push_project(){
-    echo -e "${IYellow}Pushing project: \nCommit message $1${END}"
+    echo -e "${IYellow}Pushing project$1${END}"
     git add .
     git commit -m "$1"
     git push
+    echo -e "${IYellow}\nCommit message: $1${END}"
+
 }
 
 push_project "$1"
